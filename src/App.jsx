b405b2cc15feb848e28dfa0fcfd8e8e06c01fc35
s-rezayi <rezayi.soheil@gmail.com>
2022-12-26 09:20:59 -0800

    add athentication

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import NewNote from "./features/notes/NewNote";
 import EditUser from "./features/users/EditUser";
 import NewUserForm from "./features/users/NewUserForm";
 import Prefetch from "./features/auth/PreFetch";
+import PersistLogin from "./features/auth/PersistLogin";
 
 function App() {
     return (
@@ -19,20 +20,22 @@ function App() {
             <Route index element={<Public />} />
             <Route path="login" element={<Login />} />
 
-            <Route element={<Prefetch />}>
-                <Route path="dash" element={<AdminDash />}>
-                    <Route index element={<Welcome />} />
+            <Route element={<PersistLogin />}>
+                <Route element={<Prefetch />}>
+                    <Route path="dash" element={<AdminDash />}>
+                        <Route index element={<Welcome />} />
 
-                    <Route path="users">
-                        <Route index element={<UsersList />} />
-                        <Route path=":id" element={<EditUser />} />
-                        <Route path="new" element={<NewUserForm />} />
-                    </Route>
+                        <Route path="users">
+                            <Route index element={<UsersList />} />
+                            <Route path=":id" element={<EditUser />} />
+                            <Route path="new" element={<NewUserForm />} />
+                        </Route>
 
-                    <Route path="notes">
-                        <Route index element={<NotesList />} />
-                        <Route path=":id" element={<EditNote />} />
-                        <Route path="new" element={<NewNote />} />
+                        <Route path="notes">
+                            <Route index element={<NotesList />} />
+                            <Route path=":id" element={<EditNote />} />
+                            <Route path="new" element={<NewNote />} />
+                        </Route>
                     </Route>
                 </Route>
             </Route>
